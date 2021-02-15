4ee001559586c04f52463550dde89881ee6aaa9d
jahidmahmud <mahmud.jahid519@gmail.com>
2021-02-15 22:21:57 +0600

    1st commit

---
+++ Back End/Models/Link.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickStoreAPI.Models
+{
+    public class Link
+    {
+        public string Url { get; set; }
+        public string Method { get; set; }
+        public string Relation { get; set; }
+
+    }
+}
